6505e401ac66e3859e9c501be8ffe6b1d1d9cf29
Jason Penniman <jpenniman@gmail.com>
2024-07-07 09:20:30 -0700

    winget installs

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
 # win-dev-rig
+
+1. Enable Hyper-V and Virtual Machine Platform
+
+2. Install WSL distribution
+
+```
+wsl --install -d Ubuntu
+``` 
+
+Run `./install.ps1`
---
+++ install.ps1
@@ -0,0 +1,21 @@
+winget install Bitwarden.Bitwarden
+winget install JanDeDobbeleer.OhMyPosh -s winget
+winget install Git.Git
+winget install GitKraken
+winget install JGraph.Draw
+winget install Notepad++.Notepad++
+winget install Microsoft.VisualStudioCode
+winget install Microsoft.DotNet.SDK.8
+winget install Microsoft.OpenJDK.21
+winget install JetBrains.Toolbox
+winget install dotPDN.PaintDotNet
+winget install Postman.Postman
+winget install Bruno.Bruno
+winget install Microsoft.AzureCLI
+winget install Amazon.AWSCLI
+winget install Mozilla.FireFox
+winget install Proton.ProtonVPN
+winget install Proton.ProtonDrive
+winget install Docker.DockerDesktop
+winget install Microsoft.SQLServerManagementStudio
+winget install Microsoft.VisualStudio.2022.Community
